4d4a442f108b2c8db954d0cfc9f9bf2d96690207
Vincent Gao <vincent.gao@dpc.vic.gov.au>
2020-09-17 10:19:37 +1000

    [SDPA-4613] update tide_core and dev-tools

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-# export GH_COMMIT=COMMIT_SHA
+export GH_COMMIT=49d92511c2d690c504ea275c159b13651120cc93
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
