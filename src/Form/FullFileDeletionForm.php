9b1bac5d8eac45b8df26f8a6d57b2c5ca431386e
Vincent Gao <vincent.gao@dpc.vic.gov.au>
2020-10-19 22:58:33 +1100

    only scan the current directory of media file

--- src/Form/FullFileDeletionForm.php
+++ src/Form/FullFileDeletionForm.php
@@ -3,16 +3,18 @@
 namespace Drupal\tide_media\Form;
 
 use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Datetime\DateFormatterInterface;
 use Drupal\Core\Entity\ContentEntityConfirmFormBase;
 use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\File\Exception\FileNotExistsException;
+use Drupal\Core\File\FileSystemInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\Element;
 use Drupal\Core\Url;
 use Drupal\file\Entity\File;
-use Drupal\file\FileUsage\FileUsageInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -28,18 +30,33 @@ abstract class FullFileDeletionForm extends ContentEntityConfirmFormBase {
   protected $entity;
 
   /**
-   * The File Usage Service.
+   * The file storage service.
    *
-   * @var \Drupal\file\FileUsage\FileUsageInterface
+   * @var \Drupal\file\FileStorageInterface
    */
-  protected $fileUsage;
+  protected $fileStorage;
+
+  /**
+   * The file system service.
+   *
+   * @var \Drupal\Core\File\FileSystemInterface
+   */
+  protected $fileSystem;
+
+  /**
+   * The DateFormatter service.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatterInterface
+   */
+  protected $dateFormatter;
 
   /**
    * {@inheritdoc}
    */
-  public function __construct(FileUsageInterface $file_usage, EntityRepositoryInterface $entity_repository, EntityTypeBundleInfoInterface $entity_type_bundle_info = NULL, TimeInterface $time = NULL) {
-    $this->fileUsage = $file_usage;
-
+  public function __construct(EntityTypeManagerInterface $entityTypeManager, FileSystemInterface $fileSystem, DateFormatterInterface $dateFormatter, EntityRepositoryInterface $entity_repository, EntityTypeBundleInfoInterface $entity_type_bundle_info = NULL, TimeInterface $time = NULL) {
+    $this->fileStorage = $entityTypeManager->getStorage('file');
+    $this->fileSystem = $fileSystem;
+    $this->dateFormatter = $dateFormatter;
     parent::__construct($entity_repository, $entity_type_bundle_info, $time);
   }
 
@@ -48,7 +65,9 @@ abstract class FullFileDeletionForm extends ContentEntityConfirmFormBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('file.usage'),
+      $container->get('entity_type.manager'),
+      $container->get('file_system'),
+      $container->get('date.formatter'),
       $container->get('entity.repository'),
       $container->get('entity_type.bundle.info'),
       $container->get('datetime.time'));
@@ -64,89 +83,105 @@ abstract class FullFileDeletionForm extends ContentEntityConfirmFormBase {
     if (!$file) {
       return $parent;
     }
-    // Basic settings.
-    $parent['info'] = [
-      '#type' => 'table',
-      '#header' => [
-        'to be deleting',
-        'file name',
-        'url',
-        'delete',
-        'media',
-      ],
-    ];
-    /** @var \Drupal\file\FileStorageInterface $file_storage */
-    $file_storage = \Drupal::entityTypeManager()->getStorage('file');
-    // Try to get a search keyword.
-    $key = preg_replace('/_[^_.]*\./', '.', $file->getFilename());
-    $key = pathinfo($key, PATHINFO_FILENAME);
-    // Searching from table.
-    $results = $file_storage->getQuery()
-      ->condition('filename', '%' . $key . '%', 'LIKE')
-      ->execute();
+
+    // Gets and parses uri.
+    $uri = $file->getFileUri();
+    $parsed = parse_url($uri);
+
+    // Try to get a original file name.
+    $filename = preg_replace('/_[0-9]+(\.)/', '.', $file->getFilename(), 1);
+    $file_extension = pathinfo($filename, PATHINFO_EXTENSION);
+    $original_file_name = pathinfo($filename, PATHINFO_FILENAME);
+    // // Search the directory.
+    $scanned_results = $this->fileSystem->scanDirectory($parsed['scheme'] . '://' . $parsed['host'], '/^' . $original_file_name . '(_\d+)?\\' . '.' . $file_extension . '/');
     $parent['description']['#markup'] = t('Clicking the button will delete the file entirely from the system');
-    if ($results) {
-      $results = $file_storage->loadMultiple($results);
-      /** @var \Drupal\file\FileInterface $result */
-      foreach ($results as $id => $result) {
-        $has_media = TRUE;
-        $referenced = file_get_file_references($result, NULL, EntityStorageInterface::FIELD_LOAD_REVISION, '');
-        if (empty($referenced)) {
-          $has_media = FALSE;
-        }
-        $media = new \stdClass();
-        if ($has_media) {
-          if (isset($referenced['field_media_image'])) {
-            foreach ($referenced['field_media_image']['media'] as $key => $entity) {
-              $media = $entity;
+    if ($scanned_results) {
+      // Basic settings.
+      $parent['info'] = [
+        '#type' => 'table',
+        '#header' => [
+          'to be deleting',
+          'url',
+          'created',
+          'file status',
+          'delete',
+          'media',
+        ],
+      ];
+      foreach ($scanned_results as $id => $item) {
+        /** @var \Drupal\file\Entity\File $result */
+        $result = $this->fileStorage->loadByProperties(['uri' => $id]);
+        if ($result) {
+          $result = reset($result);
+          // Checks if the user is allowed to delete.
+          if (!$result->access('delete', $this->currentUser())) {
+            continue;
+          }
+          // Gets related media entity.
+          $has_media_entity = TRUE;
+          $referenced = file_get_file_references($result, NULL, EntityStorageInterface::FIELD_LOAD_REVISION, '');
+          if (empty($referenced)) {
+            $has_media_entity = FALSE;
+          }
+          $media = new \stdClass();
+          if ($has_media_entity) {
+            if (isset($referenced['field_media_image'])) {
+              foreach ($referenced['field_media_image']['media'] as $key => $entity) {
+                $media = $entity;
+              }
+            }
+            if (isset($referenced['field_media_file'])) {
+              foreach ($referenced['field_media_file']['media'] as $key => $entity) {
+                $media = $entity;
+              }
             }
           }
-          if (isset($referenced['field_media_file'])) {
-            foreach ($referenced['field_media_file']['media'] as $key => $entity) {
-              $media = $entity;
-            }
-          }
-        }
-        $parent['info'][$id]['to_be_deleting'] = [
-          '#type' => 'checkbox',
-          '#title' => $this->t('Delete?'),
-          '#default_value' => $result->id() == $file->id() ?? TRUE,
-          '#disabled' => $result->id() == $file->id() ?? TRUE,
-        ];
-        $parent['info'][$id]['filename'] = [
-          '#markup' => $result->getFilename(),
-        ];
-        $parent['info'][$id]['uri'] = [
-          '#type' => 'link',
-          '#title' => file_create_url($result->getFileUri()),
-          '#attributes' => ['target' => '_blank'],
-          '#url' => Url::fromUri(file_create_url($result->getFileUri())),
-        ];
-        $parent['info'][$id]['delete'] = [
-          '#type' => 'link',
-          '#title' => 'Delete',
-          '#url' => Url::fromRoute('tide_media.file.delete_action', [
-            'fid' => $result->id(),
-            'base_entity_id' => $this->entity->getEntityTypeId() . '_' . $this->entity->id(),
-          ]),
-          '#access' => $result->id() !== $file->id() ?? FALSE,
-          '#ajax' => [
-            'dialogType' => 'modal',
-            'progress' => [
-              'type' => 'throbber',
-              'message' => 'Loading...',
+          // Builds the table.
+          $parent['info'][$result->id()]['to_be_deleting'] = [
+            '#type' => 'checkbox',
+            '#title' => $this->t('Delete?'),
+            '#default_value' => $result->id() == $file->id() ?? TRUE,
+            '#disabled' => $result->id() == $file->id() ?? TRUE,
+          ];
+          $parent['info'][$result->id()]['uri'] = [
+            '#type' => 'link',
+            '#title' => $result->getFilename(),
+            '#attributes' => ['target' => '_blank'],
+            '#url' => Url::fromUri(file_create_url($result->getFileUri())),
+          ];
+          $parent['info'][$result->id()]['created'] = [
+            '#markup' => $this->dateFormatter->format($result->created->value, 'custom', 'd/M/Y - H:i'),
+          ];
+          $parent['info'][$result->id()]['file_status'] = [
+            '#markup' => $result->isPermanent() ? 'Permanent' : 'Temporary',
+          ];
+
+          $parent['info'][$result->id()]['delete'] = [
+            '#type' => 'link',
+            '#title' => 'Delete',
+            '#url' => Url::fromRoute('tide_media.file.delete_action', [
+              'fid' => $result->id(),
+              'base_entity_id' => $this->entity->getEntityTypeId() . '_' . $this->entity->id(),
+            ]),
+            '#access' => $result->id() !== $file->id() ?? FALSE,
+            '#ajax' => [
+              'dialogType' => 'modal',
+              'progress' => [
+                'type' => 'throbber',
+                'message' => 'Loading...',
+              ],
             ],
-          ],
-        ];
-        $parent['info'][$id]['linked_media'] = [
-          '#type' => 'inline_template',
-          '#template' => '{% if has_media == false %}<p>{{ title }}</p>{% else %}<a href="{{ link }}" _target="_blank">{{ title }}</a>{% endif %}',
-          '#context' => [
-            'has_media' => $has_media,
-            'title' => $has_media ? $media->getName() : 'No media linked',
-            'link' => $has_media ? $media->toUrl()->toString() : NULL,
-          ],
-        ];
+          ];
+          $parent['info'][$result->id()]['linked_media'] = [
+            '#type' => 'inline_template',
+            '#template' => '{% if has_media == false %}<p>{{ title }}</p>{% else %}<a href="{{ link }}" _target="_blank">{{ title }}</a>{% endif %}',
+            '#context' => [
+              'has_media' => $has_media_entity,
+              'title' => $has_media_entity ? $media->getName() : 'No media linked',
+              'link' => $has_media_entity ? $media->toUrl()->toString() : NULL,
+            ],
+          ];
+        }
       }
     }
     return $parent;
@@ -158,16 +193,18 @@ abstract class FullFileDeletionForm extends ContentEntityConfirmFormBase {
   public function validateForm(array &$form, FormStateInterface $form_state) {
     $value = 0;
     $deleted_files = [];
-    foreach (Element::children($form['info']) as $delta) {
-      if ($form['info'][$delta]['to_be_deleting']['#value'] == 1) {
-        $deleted_files[] = $delta;
+    if (isset($form['info'])) {
+      foreach (Element::children($form['info']) as $delta) {
+        if ($form['info'][$delta]['to_be_deleting']['#value'] == 1) {
+          $deleted_files[] = $delta;
+        }
+        $value += $form['info'][$delta]['to_be_deleting']['#value'];
       }
-      $value += $form['info'][$delta]['to_be_deleting']['#value'];
+      if ($value == 0) {
+        $form_state->setError($form['info'], 'You must select at least 1 file to delete.');
+      }
+      $form_state->set('deleted_files', $deleted_files);
     }
-    if ($value == 0) {
-      $form_state->setError($form['info'], 'You must select at least 1 file to delete.');
-    }
-    $form_state->set('deleted_files', $deleted_files);
     parent::validateForm($form, $form_state);
   }
 
@@ -179,7 +216,7 @@ abstract class FullFileDeletionForm extends ContentEntityConfirmFormBase {
     if ($file_ids) {
       $files = File::loadMultiple($file_ids);
       try {
-        \Drupal::entityTypeManager()->getStorage('file')->delete($files);
+        $this->fileStorage->delete($files);
       }
       catch (FileNotExistsException $exception) {
         watchdog_exception('tide_media', $exception);
