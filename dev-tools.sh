5450337dd2141fbca996eceb2db680f6e2d3ed5e
Vincent Gao <vincent.gao@dpc.vic.gov.au>
2020-11-19 11:45:36 +1100

    Upgrade to 8.8.x

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-# export GH_COMMIT=COMMIT_SHA
+ export GH_COMMIT=5aae222dc98b27b8f34ef3c8ab66cfddd4331566
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
