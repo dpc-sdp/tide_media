9f22840192c7d276aa2719ebc71992754fe12406
vincent-gao <vincent.gao@dpc.vic.gov.au>
2021-04-01 14:07:29 +1100

    Fixes files listing issue. (#58)
    * Fixes file listing issue.
    
    * updates and fixes known issues

--- src/Form/FullFileDeletionForm.php
+++ src/Form/FullFileDeletionForm.php
@@ -9,12 +9,12 @@ use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\File\Exception\FileNotExistsException;
 use Drupal\Core\File\FileSystemInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\Element;
 use Drupal\Core\Url;
 use Drupal\file\Entity\File;
+use Drupal\media\Entity\Media;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -51,10 +51,18 @@ abstract class FullFileDeletionForm extends ContentEntityConfirmFormBase {
   protected $dateFormatter;
 
   /**
+   * The media storage service.
+   *
+   * @var \Drupal\Media\MediaStorage
+   */
+  protected $mediaStorage;
+
+  /**
    * {@inheritdoc}
    */
   public function __construct(EntityTypeManagerInterface $entityTypeManager, FileSystemInterface $fileSystem, DateFormatterInterface $dateFormatter, EntityRepositoryInterface $entity_repository, EntityTypeBundleInfoInterface $entity_type_bundle_info = NULL, TimeInterface $time = NULL) {
     $this->fileStorage = $entityTypeManager->getStorage('file');
+    $this->mediaStorage = $entityTypeManager->getStorage('media');
     $this->fileSystem = $fileSystem;
     $this->dateFormatter = $dateFormatter;
     parent::__construct($entity_repository, $entity_type_bundle_info, $time);
@@ -94,9 +102,30 @@ abstract class FullFileDeletionForm extends ContentEntityConfirmFormBase {
     $filename = preg_replace('/_[0-9]+(\.)/', '.', $file->getFilename(), 1);
     $file_extension = pathinfo($filename, PATHINFO_EXTENSION);
     $original_file_name = pathinfo($filename, PATHINFO_FILENAME);
-    // // Search the directory.
+    // Escapes special characters.
+    $original_file_name = preg_replace('/([^A-Za-z0-9\s])/', '\\\\$1', $original_file_name);
+    // Search the directory.
     $scanned_results = $this->fileSystem->scanDirectory($parsed['scheme'] . '://' . $parsed['host'], '/^' . $original_file_name . '(_\d+)?\\' . '.' . $file_extension . '/');
     $parent['description']['#markup'] = t('Clicking the button will delete the file entirely from the system');
+    $revision_ids = $this->mediaStorage->getQuery()
+      ->allRevisions()
+      ->condition('mid', $this->entity->id())
+      ->execute();
+    if ($revision_ids && count($revision_ids) > 1) {
+      foreach ($revision_ids as $revision_id => $mid) {
+        if ($revision_id == $this->entity->getRevisionId()) {
+          continue;
+        }
+        $revision = $this->mediaStorage->loadRevision($revision_id);
+        $fid = $revision->getSource()->getSourceFieldValue($revision);
+        if ($fid) {
+          $media_revision_file = File::load($fid);
+          if ($media_revision_file && !isset($scanned_results[$media_revision_file->getFileUri()])) {
+            $scanned_results[$media_revision_file->getFileUri()] = 'revision';
+          }
+        }
+      }
+    }
     if ($scanned_results) {
       // Basic settings.
       $parent['info'] = [
@@ -116,25 +145,30 @@ abstract class FullFileDeletionForm extends ContentEntityConfirmFormBase {
         if ($result) {
           $result = reset($result);
           // Checks if the user is allowed to delete.
-          if (!$result->access('delete', $this->currentUser())) {
+          // Only the file owner can update or delete the file entity,
+          // In our case, just check if the user can view the file entity.
+          if (!$result->access('view')) {
             continue;
           }
+
           // Gets related media entity.
           $has_media_entity = TRUE;
           $referenced = file_get_file_references($result, NULL, EntityStorageInterface::FIELD_LOAD_REVISION, '');
           if (empty($referenced)) {
             $has_media_entity = FALSE;
           }
-          $media = new \stdClass();
+          if ($item == 'revision') {
+            $has_media_entity = FALSE;
+          }
+          $media = NULL;
           if ($has_media_entity) {
-            if (isset($referenced['field_media_image'])) {
-              foreach ($referenced['field_media_image']['media'] as $key => $entity) {
-                $media = $entity;
-              }
-            }
-            if (isset($referenced['field_media_file'])) {
-              foreach ($referenced['field_media_file']['media'] as $key => $entity) {
-                $media = $entity;
+            foreach ($referenced as $data) {
+              foreach (array_keys($data) as $entity_type_id) {
+                if ($entity_type_id == 'media') {
+                  foreach ($data[$entity_type_id] as $key => $value) {
+                    $media = $value;
+                  }
+                }
               }
             }
           }
@@ -157,7 +191,6 @@ abstract class FullFileDeletionForm extends ContentEntityConfirmFormBase {
           $parent['info'][$result->id()]['file_status'] = [
             '#markup' => $result->isPermanent() ? 'Permanent' : 'Temporary',
           ];
-
           $parent['info'][$result->id()]['delete'] = [
             '#type' => 'dropbutton',
             '#links' => [
@@ -165,7 +198,7 @@ abstract class FullFileDeletionForm extends ContentEntityConfirmFormBase {
                 'title' => $this->t('Delete'),
                 'url' => Url::fromRoute('tide_media.file.delete_action', [
                   'fid' => $result->id(),
-                  'base_entity_id' => $this->entity->getEntityTypeId() . '_' . $this->entity->id(),
+                  'redirect_info' => $this->entity->getEntityTypeId() . '_' . $this->entity->id(),
                 ]),
               ],
             ],
@@ -199,10 +232,15 @@ abstract class FullFileDeletionForm extends ContentEntityConfirmFormBase {
   public function validateForm(array &$form, FormStateInterface $form_state) {
     $value = 0;
     $deleted_files = [];
+    $deleted_media = [];
     if (isset($form['info'])) {
       foreach (Element::children($form['info']) as $delta) {
         if ($form['info'][$delta]['to_be_deleting']['#value'] == 1) {
           $deleted_files[] = $delta;
+          preg_match("/\/(\d+)$/", $form['info'][$delta]['linked_media']['#context']['link'], $matches);
+          if (isset($matches[1]) && is_numeric($matches[1])) {
+            $deleted_media[] = $matches[1];
+          }
         }
         $value += $form['info'][$delta]['to_be_deleting']['#value'];
       }
@@ -210,6 +248,7 @@ abstract class FullFileDeletionForm extends ContentEntityConfirmFormBase {
         $form_state->setError($form['info'], 'You must select at least 1 file to delete.');
       }
       $form_state->set('deleted_files', $deleted_files);
+      $form_state->set('deleted_media', $deleted_media);
     }
     parent::validateForm($form, $form_state);
   }
@@ -219,12 +258,22 @@ abstract class FullFileDeletionForm extends ContentEntityConfirmFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $file_ids = $form_state->get('deleted_files');
+    $media_ids = $form_state->get('deleted_media');
     if ($file_ids) {
       $files = File::loadMultiple($file_ids);
       try {
         $this->fileStorage->delete($files);
       }
-      catch (FileNotExistsException $exception) {
+      catch (\Exception $exception) {
+        watchdog_exception('tide_media', $exception);
+      }
+    }
+    if ($media_ids) {
+      $media = Media::loadMultiple($media_ids);
+      try {
+        $this->mediaStorage->delete($media);
+      }
+      catch (\Exception $exception) {
         watchdog_exception('tide_media', $exception);
       }
     }
