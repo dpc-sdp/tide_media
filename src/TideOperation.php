677bee380d5d776c538b04411793b9dab964abd8
MdNadimHossain <getovic@gmail.com>
2021-08-30 16:53:54 +1000

    [SDPA-5411] Added entity usage module.

--- src/TideOperation.php
+++ src/TideOperation.php
@@ -8,6 +8,25 @@ namespace Drupal\tide_media;
 class TideOperation {
 
   /**
+   * Helper to install a module.
+   *
+   * @param string $module
+   *   Module name.
+   *
+   * @throws \Exception
+   *   When module already installed.
+   */
+  public function tide_media_install_module($module) {
+    /** @var \Drupal\Core\Extension\ModuleHandler $moduleHandler */
+    $moduleExists = \Drupal::service('module_handler')->moduleExists($module);
+    // Check if module is both installed and enabled.
+    if (!$moduleExists) {
+      // If not, install the queue_mail module.
+      \Drupal::service('module_installer')->install([$module]);
+    }
+  }
+
+  /**
    * Enables standalone media URL for video transcripts.
    */
   public static function enableStandaloneMedia() {
@@ -15,6 +34,13 @@ class TideOperation {
     $config = $config_factory->getEditable('media.settings');
     $config->set('standalone_url', TRUE);
     $config->save();
+  }
+
+  /**
+   * Enables entity_usage module.
+   */
+  public function enable_entity_usage() {
+    $this->tide_media_install_module('entity_usage');
   }
 
 }
