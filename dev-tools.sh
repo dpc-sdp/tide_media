a45e09d47b9a89a7e4e14594a7fb73f1149b9723
Nadim Hossain <getovic@gmail.com>
2022-07-06 16:58:52 +1000

    Added dev-tools hash.

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-# export GH_COMMIT=COMMIT_SHA
+export GH_COMMIT=fdc80e5c45c794d9d466c505aac88398173f8b23
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
