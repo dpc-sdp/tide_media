845ca60ce3668f29f36d48bb102785698d5cdc68
Nadim Hossain <getovic@gmail.com>
2022-07-13 21:30:55 +1000

    Updated js.

--- js/embed_media_back_button.js
+++ js/embed_media_back_button.js
@@ -14,25 +14,29 @@
       if (!window.CKEDITOR) {
         return;
       }
-      let iframePath, itemName, licenseType, mediaType, published, site
+      let iframePath, itemName, licenseType, mediaType, status, site
       const iframeName = 'entity_browser_iframe_tide_media_browser_iframe'
       const homePagePath = '/entity-browser/iframe/tide_media_browser_iframe'
       const backButton = $('#entity-embed-dialog-tide-media-browser-home')
       $(document).ready(function () {
-        // Remove the name field value on modal first load if any.
-        localStorage.removeItem("tideMediaBrowsernameFilterVal");
-        $("iframe").on("load", function() {
-          if ($("iframe") && $("iframe").length > 0) {
-            for (let i = 0; i < $("iframe").length; i++) {
-              if ($("iframe")[i].name && $("iframe")[i].name === iframeName) {
-                iframePath = $("iframe")[i].contentWindow.location.pathname
+        // Remove the filter values on modal first load if any.
+        localStorage.removeItem('tideMediaBrowserNameFilterVal');
+        localStorage.removeItem('tideMediaBrowserLicenseTypeFilterVal');
+        localStorage.removeItem('tideMediaBrowserMediaTypeFilterVal');
+        localStorage.removeItem('tideMediaBrowserStatusFilterVal');
+        localStorage.removeItem('tideMediaBrowserSiteFilterVal');
+        $('iframe').on('load', function() {
+          if ($('iframe') && $('iframe').length > 0) {
+            for (let i = 0; i < $('iframe').length; i++) {
+              if ($('iframe')[i].name && $('iframe')[i].name === iframeName) {
+                iframePath = $('iframe')[i].contentWindow.location.pathname
                 // Shows the back button when iframe is not in the first page.
                 if (iframePath && iframePath !== homePagePath) {
-                  backButton.css("display", "block");
+                  backButton.css('display', 'block');
                 }
                 // Hides the back button for the first page.
                 if (iframePath && iframePath === homePagePath) {
-                  backButton.css("display", "none");
+                  backButton.css('display', 'none');
                 }
               }
             }
@@ -41,42 +45,44 @@
       });
       // Disable back button when it is on the first page.
       backButton.click(function() {
-        iframePath = $("iframe")[1].contentWindow.location.pathname
-        if (iframePath && iframePath !== homePagePath) {
-          $("iframe")[1].contentWindow.history.back()
-        }
-        $("iframe").on("load", function() {
-          if ($("iframe") && $("iframe").length > 0) {
-            for (let i = 0; i < $("iframe").length; i++) {
-              if ($("iframe")[i].name && $("iframe")[i].name === iframeName) {
-                // Get the filter values on iframe load.
-                itemName =  localStorage.getItem("tideMediaBrowsernameFilterVal")
-                $(this).contents().find('#edit-name').val(itemName)
-                licenseType = $(this).contents().find('#edit-field-license-type-target-id-1 :selected').text()
-                mediaType = $(this).contents().find('#edit-bundle :selected').text()
-                published = $(this).contents().find('#edit-status :selected').text()
-                site = $(this).contents().find('#edit-field-media-site-target-id :selected').text()
-                // Trigger ajax call submit to retrive the user search for current session.
-                // Will only trigger if there is any filter value set for this session.
-                if (checkFilterValuesOnLoad (itemName, licenseType, mediaType, published, site)) {
-                  $(this).contents().find('#edit-submit-tide-media-browser').trigger('click');
-                }
-              }
-            }
+        history.back()
+        $('iframe').on('load', function() {
+          // Get the filter values on iframe load.
+          itemName = localStorage.getItem('tideMediaBrowserNameFilterVal')
+          $(this).contents().find('input[name=name]').val(itemName)
+          licenseType = checkFilterValueInStorage('tideMediaBrowserLicenseTypeFilterVal')
+          $(this).contents().find('select[name=field_license_type_target_id_1]').val(licenseType)
+          mediaType = checkFilterValueInStorage('tideMediaBrowserMediaTypeFilterVal')
+          $(this).contents().find('select[name=bundle]').val(mediaType)
+          status = checkFilterValueInStorage('tideMediaBrowserStatusFilterVal')
+          $(this).contents().find('select[name=status]').val(status)
+          site = checkFilterValueInStorage('tideMediaBrowserSiteFilterVal')
+          $(this).contents().find('select[name=field_media_site_target_id]').val(site)
+          // Trigger ajax call submit to retrive the user search for current session.
+          // Will only trigger if there is any filter value set for this session.
+          if (checkFilterValuesOnLoad (itemName, licenseType, mediaType, status, site)) {
+            $(this).contents().find('input[value=Apply]').trigger('click');
           }
         })
       });
       // Check filter values on load.
-      function checkFilterValuesOnLoad (itemName, licenseType, mediaType, published, site) {
-        const defaultValue = "- Any -"
+      function checkFilterValuesOnLoad (itemName, licenseType, mediaType, status, site) {
+        const defaultValue = "All"
         if ((itemName && itemName !== 'undefined') ||
           (licenseType && licenseType !== defaultValue) || 
           (mediaType && mediaType !== defaultValue) || 
-          (published && published !== defaultValue) || 
+          (status && status !== defaultValue) || 
           (site && site !== defaultValue)) {
             return true
         }
         return false
+      }
+      // Value check on localStorage, if empty set to default.
+      function checkFilterValueInStorage (filterValName) {
+        if (localStorage.getItem(filterValName)) {
+          return localStorage.getItem(filterValName)
+        }
+        return 'All'
       }
     },
   };
--- js/embed_media_iframe_form.js
+++ js/embed_media_iframe_form.js
@@ -6,21 +6,26 @@
  * comes back to the first page.
  */
 
-(function ($, Drupal) {
-    'use strict';
+ (function ($, Drupal) {
+  'use strict';
 
-    Drupal.behaviors.tide_media_media_form = {
-      attach: function (context, settings) {
-        const nameField = $('#views-exposed-form-tide-media-browser-media-browser input[name=name]')
-        const formSubmit = $('#views-exposed-form-tide-media-browser-media-browser input[type=submit]')
-        $(document).ready(function () {
-          // Retrieving value from session and setting it to the name field.
-          nameField.val(localStorage.getItem("tideMediaBrowsernameFilterVal"))
-        })
-        // On every submit it will save the namefield value to the localstorage.
-        formSubmit.click(function () {
-          localStorage.setItem("tideMediaBrowsernameFilterVal", nameField.val());
-        })
-      }
-    };
+  Drupal.behaviors.tide_media_media_form = {
+    attach: function (context, settings) {
+      const nameField = $('#views-exposed-form-tide-media-browser-media-browser input[name=name]')
+      const licenseType = $('#views-exposed-form-tide-media-browser-media-browser select[name=field_license_type_target_id_1]')
+      const mediaType = $('#views-exposed-form-tide-media-browser-media-browser select[name=bundle]')
+      const status = $('#views-exposed-form-tide-media-browser-media-browser select[name=status]')
+      const site = $('#views-exposed-form-tide-media-browser-media-browser select[name=field_media_site_target_id]')
+      const formSubmit = $('#views-exposed-form-tide-media-browser-media-browser input[type=submit]')
+      // On every submit it will save the namefield value to the localstorage.
+      formSubmit.click(function () {
+        // Store user set values on every click.
+        localStorage.setItem('tideMediaBrowserNameFilterVal', nameField.val());
+        localStorage.setItem('tideMediaBrowserLicenseTypeFilterVal', licenseType.val());
+        localStorage.setItem('tideMediaBrowserMediaTypeFilterVal', mediaType.val());
+        localStorage.setItem('tideMediaBrowserStatusFilterVal', status.val());
+        localStorage.setItem('tideMediaBrowserSiteFilterVal', site.val());
+      })
+    }
+  };
 }(jQuery, Drupal));
