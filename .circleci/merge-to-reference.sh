9722c0a50655d4b28c433b6047bb7d4df8a53c20
Edward Yuen <edward.yuen@dpc.vic.gov.au>
2022-12-08 13:50:32 +1100

    Updates tide media for merge to reference settings.

--- .circleci/merge-to-reference.sh
+++ .circleci/merge-to-reference.sh
@@ -7,8 +7,12 @@ echo "==> Checking out reference branch"
 git checkout reference
 echo "==> Merging develop to reference branch"
 git merge develop
-echo "==> Making sure composer.json is unchanged"
-git checkout composer.json
+git checkout --theirs composer.json
+git add .
+echo "==> Replacing composer require entries starting with dpc-sdp with value dev-reference"
+cat composer.json | gojq '.require |= with_entries(
+  if (.key | test("dpc-sdp/tide_site_restriction"))
+  then .value = "dev-reference" else . end)' > composer.json.backup && mv -f composer.json.backup composer.json
 echo "==> Add all changes"
 git add .
 git commit -m "Merge changes from develop."
