53cc49c16df0e6a2ba59808c5f26c5e9a29f6873
Edward Yuen <edward.yuen@dpc.vic.gov.au>
2023-06-23 12:05:56 +1000

    [SRM-939] Upgrade to D10 compatible.

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-# export GH_COMMIT=COMMIT_SHA
+export GH_COMMIT=1d7967e6f9e4d2fb4dc7ca701a14872b8277cee0
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
