572f4aaf778a1ab3942909b778b0d1ca00e3d1f7
Edward Yuen <edward.yuen@dpc.vic.gov.au>
2023-06-29 09:29:37 +1000

    Moves permissions. Clean up update hooks.

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-export GH_COMMIT=1d7967e6f9e4d2fb4dc7ca701a14872b8277cee0
+export GH_COMMIT=e57446d1406c2c345d7e87a70a23ea58163ad619
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
